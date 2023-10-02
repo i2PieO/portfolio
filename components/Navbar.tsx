790d2cfc3f9c83caae1d5f704ff5e2b5d56fbdf5
i2pieo <jawadsyed406@gmail.com>
2023-10-02 18:01:18 +0300

    portfolio

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,26 @@
 import Link from "next/link";
 import {useRef} from "react"
+import { useState } from "react";
 import { delay, motion } from "framer-motion";
+import { MdOutlineClose } from "react-icons/md"; 
 
 const Navbar = () => {
-    const ref = useRef<string | any>("")
+    const ref = useRef<string | any>("");
+    const [showMenu, setShowMenu] = useState(false);
     const handleScroll =(e:React.MouseEvent<HTMLAnchorElement, MouseEvent>)=>{
       e.preventDefault();
-    const href = e.currentTarget.herf;
+    const href = e.currentTarget.href;
     const targetId = href.replace(/.*\#/, "");
-    const elem = 
+    const elem =document.getElementById(targetId)
+    elem?.scrollIntoView({
+      behavior: "smooth",
+    });
+    //Update the class name of the clicked link
+    const links = document.querySelectorAll(".nav-link")
+    links.forEach((link)=>{
+      link.classList.remove("active")
+    });
+    e.currentTarget.classList.add("active");
     };
 
     return(
@@ -106,7 +118,9 @@ const Navbar = () => {
               </motion.button>
             </div>
             {/* Small Icon Section */}
-            <div className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden
+            <div
+            onClick={() => setShowMenu(true)}
+            className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden
             text-4xl text-textGreen cursor-pointer overflow-hidden group">
               <span className="w-full h-[2px] bg-textGreen inline-flex transform
               group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
@@ -117,6 +131,26 @@ const Navbar = () => {
               translate-x-1 group-hover:translate-x-3 transition-all ease-in-out 
               duration-300"></span> 
             </div>
+            {
+              showMenu && (
+                <div ref={(node) => (ref.current = node)}
+                className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black
+                bg-opacity-50 flex flex-col items-end">
+                <motion.div
+                  initial={{ x:20, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.1 }}
+                  className="w-[80%] h-full overflow-y-scroll scrollbarHide bg-[#112240]
+                  flex flex-col items-center px-4 py-10 relative"
+                  >
+                    <MdOutlineClose 
+                    onClick={() => setShowMenu(false)}
+                    className="text-3xl text-textGreen curson-pointer 
+                    hover:text-red-500 absolute top-4 right-4" />
+                  </motion.div>
+                </div>
+              )
+            }
           </div>
         </div>
         );
