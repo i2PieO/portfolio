67dd342cea4ed36f25815834bb77742db58e512f
i2pieo <jawadsyed406@gmail.com>
2023-10-14 19:04:47 +0300

    gap

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RightSide = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
+    <div className="w-full h-full flex flex-col items-center justify-end gap-12 text-textLight">
       <a href="mailto:jawadsyed406@gmail.com">
         <p className="text-sm font-extraFont rotate-90 w-72 tracking-wide text-textGreen">
           jawadsyed406@gmail.com
