a42d637c6c4c477e51a4f40c96fc442b8170f82d
i2pieo <jawadsyed406@gmail.com>
2023-10-14 19:29:05 +0300

    icons

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -9,7 +9,7 @@ const LeftSide = () => {
     <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
       <div className="flex flex-col gap-4">
         <a href="https://github.com/i2PieO" target="_blank">
-          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <span className="w-10 h-10 text-2xl inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <TbBrandGithub />
           </span>
         </a>
@@ -17,12 +17,12 @@ const LeftSide = () => {
           href="https://www.linkedin.com/in/syed-khundmir-jawad-76352822b/"
           target="_blank"
         >
-          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <span className="w-10 h-10 text-xl inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <SlSocialLinkedin />
           </span>
         </a>
         <a href="https://www.instagram.com/i2pieo/" target="_blank">
-          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <span className="w-10 h-10 text-xl inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <SlSocialInstagram />
           </span>
         </a>
