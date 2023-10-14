39a50b68ec5911fe4331828c92aadb4ca761a617
i2pieo <jawadsyed406@gmail.com>
2023-10-14 18:19:01 +0300

    portfolio

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const RightSide = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
+    <div className="w-full h-full flex flex-col items-center justify-end gap-40 text-textLight">
       <a href="mailto:jawadsyed406@gmail.com">
         <p className="text-sm font-extraFont rotate-90 w-72 tracking-widest text-textGreen">
           jawadsyed406@gmail.com
         </p>
       </a>
-      <span className="w-[2px] h-32 bg-textDark inline-flex"></span>
+      <span className="w-[0.1px] h-32 bg-textDark inline-flex"></span>
     </div>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Fira+Mono&family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />
