03dcbebcc4a43c995ad801b1309a106312140289
i2pieo <jawadsyed406@gmail.com>
2023-10-21 17:24:19 +0300

    sticky

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     }
   }
   return (
-    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] top-0 z-50 bg-bodyColor px-4">
+    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
       <div className="max-w-container h-full mx-auto py-1 font-extraFont flex items-center justify-between ">
         {/* ============ Logo Start here ============ */}
         <Link href="/">
